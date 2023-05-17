5b2a8a074a8d292f421a3dae5942a46cd3418c4b
Lucas-Mol <lucasmolpro@outlook.com>
2023-05-17 11:59:12 -0300

    [PROJECT]: initializing the backend server project with its dependencies and configs

---
+++ server/src/server.ts
@@ -0,0 +1,23 @@
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+
+  return users
+})
+
+app.get('/', () => {
+  return 'Hello World 🌍'
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🚀 HTTP server running on http://localhost:3333')
+  })
