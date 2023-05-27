38921783f6cc150c300f4b037c96a0b1d32a0934
Lucas-Mol <lucasmolpro@outlook.com>
2023-05-27 12:12:22 -0300

    feat(server.ts): importing and registring new AuthRoutes

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,14 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
 app.register(cors, {
   origin: true, // to do
 })
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app.get('/', () => {
