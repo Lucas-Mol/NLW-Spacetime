9a204742566bd267b0ac154a3695abc09bc62e1e
Lucas-Mol <lucasmolpro@outlook.com>
2023-05-29 20:55:29 -0300

    [web]-feat(page.tsx): updating main page to verify authentication and change component SignIn to Profile

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,19 +1,25 @@
+import { cookies } from 'next/headers'
+
+// Components
 import { Copyright } from '@/components/Copyright'
 import { EmptyMemories } from '@/components/EmptyMemories'
 import { Hero } from '@/components/Hero'
 import { PurpleBlurBackground } from '@/components/PurpleBlurBackground'
 import { SignIn } from '@/components/SignIn'
-import { Stripes } from '@/components/Stripes'
+import { StripesBackground } from '@/components/StripesBackground'
+import { Profile } from '@/components/Profile'
 
 export default function Home() {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <main className="grid min-h-screen grid-cols-2">
       {/* Left */}
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
         <PurpleBlurBackground />
-        <Stripes />
+        <StripesBackground />
 
-        <SignIn />
+        {isAuthenticated ? <Profile /> : <SignIn />}
 
         <Hero />
 
