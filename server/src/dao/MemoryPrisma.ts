a9948eca3e4ee6cc1fd588c9cace8ee8572429ce
Lucas-Mol <lucasmolpro@outlook.com>
2023-05-29 20:45:59 -0300

    [server]-refactor(MomeryPrisma): extracting all database logic methods to a new file

---
+++ server/src/dao/MemoryPrisma.ts
@@ -0,0 +1,56 @@
+import { prisma } from '../lib/prisma'
+
+export async function getMemoriesByUserId(userId: string) {
+  return await prisma.memory.findMany({
+    where: {
+      userId,
+    },
+    orderBy: { createdAt: 'asc' },
+  })
+}
+export async function getMemoryByIdOrThrow(id: string) {
+  return await prisma.memory.findUniqueOrThrow({
+    where: {
+      id,
+    },
+  })
+}
+export async function createMemory(
+  content: string,
+  coverUrl: string,
+  isPublic: boolean,
+  userId: string,
+) {
+  return await prisma.memory.create({
+    data: {
+      content,
+      coverUrl,
+      isPublic,
+      userId,
+    },
+  })
+}
+export async function updateMemory(
+  id: string,
+  content: string,
+  coverUrl: string,
+  isPublic: boolean,
+) {
+  return await prisma.memory.update({
+    where: {
+      id,
+    },
+    data: {
+      content,
+      coverUrl,
+      isPublic,
+    },
+  })
+}
+export async function removeMemory(id: string) {
+  return await prisma.memory.delete({
+    where: {
+      id,
+    },
+  })
+}
