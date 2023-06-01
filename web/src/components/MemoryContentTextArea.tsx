badf7eeb0b627e4339ab67781703e9812509d42f
Lucas-Mol <lucasmolpro@outlook.com>
2023-06-01 20:46:57 -0300

    [web]-fix(MemoryContentTextArea): fixing placeholder

--- web/src/components/MemoryContentTextArea.tsx
+++ web/src/components/MemoryContentTextArea.tsx
@@ -4,7 +4,7 @@ export function MemoryContentTextArea() {
       name="content"
       spellCheck="false"
       className="w-full flex-1 resize-none rounded border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100 placeholder:text-gray-400 focus:ring-0"
-      placeholder="Fique livre pára adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
+      placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
     />
   )
 }
