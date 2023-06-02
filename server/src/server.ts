23f691a06f84bff56c372c7b1f67bb25008bbaf2
Lucas-Mol <lucasmolpro@outlook.com>
2023-06-02 17:58:49 -0300

    [server]-feat(server.ts): adding 2 new dependencies on fastify and setting /uploads as public folder

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,21 @@
 import 'dotenv/config'
-import jwt from '@fastify/jwt'
 import fastify from 'fastify'
+import jwt from '@fastify/jwt'
 import cors from '@fastify/cors'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+
+app.register(multipart)
 
 app.register(cors, {
   origin: true, // to do
