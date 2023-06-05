e1ce73ce04cb377c390ed7c958575b8f5b70e158
Lucas-Mol <lucasmolpro@outlook.com>
2023-06-05 13:25:55 -0300

    [web]-feat-refactor(FormNewMemory): using new component exposed as 'use client' and adding submit logic within

---
+++ web/src/components/FormNewMemory.tsx
@@ -0,0 +1,95 @@
+'use client'
+
+import { api } from '@/lib/api'
+
+import { FormEvent } from 'react'
+import { useRouter } from 'next/navigation'
+
+import { MediaInput } from './MediaInput'
+import { MemoryContentTextArea } from './MemoryContentTextArea'
+import { PublicCheckbox } from './PublicCheckbox'
+import { TransparentMediaPickerAndPreview } from './TransparentMediaPickerAndPreview'
+import { SubmitButton } from './SubmitButton'
+
+import Cookie from 'js-cookie'
+
+export function FormNewMemory() {
+  const router = useRouter()
+
+  async function handleCreateMemory(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+
+    await createMemory(formData)
+
+    router.push('/')
+  }
+
+  const mediaInputId = 'media'
+
+  return (
+    <form onSubmit={handleCreateMemory} className="flex flex-1 flex-col gap-2">
+      <div className="flex items-center gap-4">
+        <MediaInput mediaInputId={mediaInputId} />
+
+        <PublicCheckbox />
+      </div>
+
+      <TransparentMediaPickerAndPreview mediaInputId={mediaInputId} />
+
+      <MemoryContentTextArea />
+
+      <SubmitButton text="Salvar" />
+    </form>
+  )
+}
+
+async function createMemory(formData: FormData) {
+  const token = `Bearer ${Cookie.get('token')}`
+  const fileToUpload = formData.get('coverUrl')
+
+  let coverUrl: string = ''
+
+  if (fileToUpload) {
+    coverUrl = await getCoverUrlFromUploadedFile(fileToUpload, token)
+  }
+
+  await postNewMemory(formData, coverUrl, token)
+}
+
+async function getCoverUrlFromUploadedFile(
+  fileEntry: string | File,
+  token: string,
+) {
+  const uploadFormData = new FormData()
+  uploadFormData.set('file', fileEntry)
+
+  const uploadResponse = await api.post('/upload', uploadFormData, {
+    headers: {
+      Authorization: token,
+    },
+  })
+
+  return uploadResponse.data.fileUrl
+}
+
+async function postNewMemory(
+  formData: FormData,
+  coverUrl: string,
+  token: string,
+) {
+  api.post(
+    '/memories',
+    {
+      coverUrl,
+      content: formData.get('content'),
+      isPublic: formData.get('isPublic'),
+    },
+    {
+      headers: {
+        Authorization: token,
+      },
+    },
+  )
+}
