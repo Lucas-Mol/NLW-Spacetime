40bd46c9181f4122bb194444cbd1228f7e097562
Lucas-Mol <lucasmolpro@outlook.com>
2023-06-05 13:22:59 -0300

    [web]-refactor(MediaInput): removing input from MediaInput and adding props

--- web/src/components/MediaInput.tsx
+++ web/src/components/MediaInput.tsx
@@ -1,14 +1,17 @@
 import { Camera } from 'lucide-react'
 
-export function MediaInput() {
+interface Props {
+  mediaInputId: string
+}
+
+export function MediaInput(props: Props) {
   return (
     <label
-      htmlFor="media"
+      htmlFor={props.mediaInputId}
       className="flex cursor-pointer items-center gap-1.5 text-sm text-gray-200 hover:text-gray-100"
     >
       <Camera className="2-4 h-4" />
       Anexar mídia
-      <input type="file" id="media" className="invisible w-0" />
     </label>
   )
 }
