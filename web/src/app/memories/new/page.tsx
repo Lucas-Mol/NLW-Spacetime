1714f98301c8a74c9e22a409e1b7e53ebc11b445
Lucas-Mol <lucasmolpro@outlook.com>
2023-06-05 13:21:47 -0300

    [web]-refactor(new/page.tsx): componentize form to just 'use client' on it

--- web/src/app/memories/new/page.tsx
+++ web/src/app/memories/new/page.tsx
@@ -1,22 +1,12 @@
 import { BackwardLink } from '@/components/BackwardLink'
-import { MediaInput } from '@/components/MediaInput'
-import { MemoryContentTextArea } from '@/components/MemoryContentTextArea'
-import { PublicCheckbox } from '@/components/PublicCheckbox'
+import { FormNewMemory } from '@/components/FormNewMemory'
 
 export default function NewMemory() {
   return (
     <div className="flex flex-1 flex-col gap-4">
       <BackwardLink url="/" text="Voltar à timeline"></BackwardLink>
 
-      <form className="flex flex-1 flex-col gap-2">
-        <div className="flex items-center gap-4">
-          <MediaInput />
-
-          <PublicCheckbox />
-        </div>
-
-        <MemoryContentTextArea />
-      </form>
+      <FormNewMemory />
     </div>
   )
 }
