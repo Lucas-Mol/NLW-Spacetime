f2361711d6ca9d8137775ccb23cf41266b4eeb66
Lucas-Mol <lucasmolpro@outlook.com>
2023-06-05 20:15:58 -0300

    [web]-fix(new/page): adding padding local instead of previous 'global' one

--- web/src/app/memories/new/page.tsx
+++ web/src/app/memories/new/page.tsx
@@ -3,7 +3,7 @@ import { FormNewMemory } from '@/components/FormNewMemory'
 
 export default function NewMemory() {
   return (
-    <div className="flex flex-1 flex-col gap-4">
+    <div className="flex flex-1 flex-col gap-4 p-16">
       <BackwardLink url="/" text="Voltar à timeline"></BackwardLink>
 
       <FormNewMemory />
