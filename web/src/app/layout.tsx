c54bb4e9e1877a4154ee60630c85d378fac46afb
Lucas-Mol <lucasmolpro@outlook.com>
2023-06-06 20:24:15 -0300

    [web]-feat(): customizing scrollbar on MemoryContextTextArea and Layout

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           </div>
 
           {/* Right */}
-          <div className="flex max-h-screen flex-col overflow-y-scroll bg-[url(../assets/bg-stars.svg)] bg-cover">
+          <div className="flex max-h-screen flex-col overflow-y-scroll bg-[url(../assets/bg-stars.svg)] bg-cover scrollbar-thin scrollbar-track-transparent scrollbar-thumb-gray-600 scrollbar-thumb-rounded-lg scrollbar-corner-rounded-lg">
             {children}
           </div>
         </main>
--- web/src/components/MemoryContentTextArea.tsx
+++ web/src/components/MemoryContentTextArea.tsx
@@ -3,7 +3,7 @@ export function MemoryContentTextArea() {
     <textarea
       name="content"
       spellCheck="false"
-      className="w-full flex-1 resize-none rounded border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100 placeholder:text-gray-400 focus:ring-0"
+      className="w-full flex-1 resize-none rounded border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100 scrollbar-thin scrollbar-track-transparent scrollbar-thumb-gray-600 scrollbar-thumb-rounded-lg scrollbar-corner-rounded-lg placeholder:text-gray-400 focus:ring-0"
       placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
     />
   )
